c8fd3a85d9a8ed4ce0d7fe935e79461d59ae1bc0
jpamperin <89536434+jpamperin@users.noreply.github.com>
2021-09-03 10:14:33 -0500

    Add project files.

---
+++ Project3/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	std::cout << "Hello World!\n";
+	return 0;
+}
+
+//Run program: Ctrl + F5 or debug > Start Without Debugging menu 
+// Debug program: F5 or Debug > Start Debugging menu
